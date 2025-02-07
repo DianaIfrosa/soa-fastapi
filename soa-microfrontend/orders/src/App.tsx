e0b209f1a5971c3d8dba8397d3ba401f1bd1d4f0
DianaIfrosa <diana.ifrosa@gmail.com>
2025-02-07 15:16:57 +0200

    fixed individual pags on separate ports for orders and products

--- soa-microfrontend/orders/src/App.tsx
+++ soa-microfrontend/orders/src/App.tsx
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom/client";
 import Order from "./Order";
 import "./index.css";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { id: 1, name: 'Laptop', price: 1299 },
+  { id: 2, name: 'Smartphone', price: 799 },
+  { id: 3, name: 'Headphones', price: 199 },
+  { id: 4, name: 'Monitor', price: 299 },
+]; 
+
 const App = () => (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    {/* <Order basket = {[]} onOrderSuccess={}/> */}
-    Hello!
+    <Order basket = {products} onOrderSuccess={() => {
+    console.log('Order successful!');
+    } }/>
     </div>
   );
 
--- soa-microfrontend/products/src/App.tsx
+++ soa-microfrontend/products/src/App.tsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 import ProductsList from "./ProductsList";
 import "./index.css";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { id: 1, name: 'Laptop', price: 1299 },
+  { id: 2, name: 'Smartphone', price: 799 },
+  { id: 3, name: 'Headphones', price: 199 },
+  { id: 4, name: 'Monitor', price: 299 },
+]; 
+
 const App = () => (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
       {/* <div>Name: products</div>
       <div>Framework: react-18</div> */}
-      <ProductsList/>
+      <ProductsList basket = {products} addToBasket={() => {
+      console.log('Add successful!');
+    } }/>
     </div>
   );
 
